d40f0f78ca2f4087bf2929704718eaf5d687a950
Martin Georgiev <geeorgiev@gmail.com>
2017-02-25 18:37:20 +0200

    Added new links and modified welcome page.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        {{ config('app.name', 'Personal finance 1.0') }}
                     </a>
                 </div>
 
